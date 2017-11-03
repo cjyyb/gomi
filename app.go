f270bf7da54815b8e7961cd07a40e28d89aa7610
yubiao <yubiao@tenxcloud.com>
2017-11-03 14:17:08 +0800

    silent change

--- app.go
+++ app.go
@@ -2,7 +2,10 @@ package gomi
 
 import (
 	"gomi/iType"
+	"net"
 	"net/http"
+
+	"github.com/golang/glog"
 )
 
 var overall iType.ExtendMiddleSlice
@@ -12,6 +15,26 @@ func init() {
 
 //App ...
 type App struct {
+	server *http.Server
+}
+
+func (a *App) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	var passage iType.BindMiddle
+	if len(overall) != 0 {
+		passage = iType.CombineMiddle(overall)
+	}
+	ctx := iType.CtxPool.Get().(*iType.Ctx)
+	defer iType.CtxPool.Put(ctx)
+	passage(ctx)
+}
+
+//New ...
+func New() *App {
+	app := App{
+		server: new(http.Server),
+	}
+	app.server.Handler = &app
+	return &app
 }
 
 //Use ...
@@ -20,17 +43,19 @@ func (a *App) Use(m ...iType.Middle) {
 }
 
 //Run ...
-func (a *App) Run(addr string) {
-	var passage iType.BindMiddle
-	if len(overall) != 0 {
-		passage = iType.CombineMiddle(overall)
+func (a *App) Run(addr string) error {
+	l, err := newListener(addr)
+	if err != nil {
+		glog.Errorln("Start http server failed, err: %v", err)
+		panic(err)
 	}
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		ctx := iType.CtxPool.Get().(*iType.Ctx)
-		ctx.Req = r
-		ctx.Res = &w
-		passage(ctx)
-		iType.CtxPool.Put(ctx)
-	})
-	http.ListenAndServe(addr, nil)
+	return a.server.Serve(l)
+}
+
+func newListener(addr string) (net.Listener, error) {
+	l, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	return l, nil
 }
